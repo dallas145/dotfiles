ab74eef7cf64f04a4ae1eb99f20017b0a0ee83d8
dallas145 <mikelg512@gmail.com>
2024-09-22 19:09:03 +0800

    add config
    lsd omp yazi

---
+++ private_dot_config/yazi/init.lua
@@ -0,0 +1,77 @@
+-- full-border
+require("full-border"):setup()
+
+-- git setup
+require("git"):setup()
+
+-- show symlink in status bar
+function Status:name()
+	local h = self._tab.current.hovered
+	if not h then
+		return ui.Line {}
+	end
+
+	local linked = ""
+	if h.link_to ~= nil then
+		linked = " -> " .. tostring(h.link_to)
+	end
+	return ui.Line(" " .. h.name .. linked)
+end
+
+-- show user/group of files in status bar
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line {}
+	end
+	return ui.Line {
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
+	}
+end, 500, Status.RIGHT)
+
+-- show username and hostname in header
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ui.Line {}
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
+
+-- You can configure your bookmarks by lua language
+local bookmarks = {}
+
+local path_sep = package.config:sub(1, 1)
+local home_path = ya.target_family() == "windows" and os.getenv("USERPROFILE") or os.getenv("HOME")
+if ya.target_family() == "windows" then
+  table.insert(bookmarks, {
+    tag = "Scoop Local",
+    
+    path = (os.getenv("SCOOP") or home_path .. "\\scoop") .. "\\",
+    key = "p"
+  })
+  table.insert(bookmarks, {
+    tag = "Scoop Global",
+    path = (os.getenv("SCOOP_GLOBAL") or "C:\\ProgramData\\scoop") .. "\\",
+    key = "P"
+  })
+end
+table.insert(bookmarks, {
+  tag = "Desktop",
+  path = home_path .. path_sep .. "Desktop" .. path_sep,
+  key = "d"
+})
+
+require("yamb"):setup {
+  -- Optional, the path ending with path seperator represents folder.
+  bookmarks = bookmarks,
+  -- Optional, the cli of fzf.
+  cli = "fzf",
+  -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+  keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  -- Optional, the path of bookmarks
+  path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark") or
+        (os.getenv("HOME") .. "/.config/yazi/bookmark"),
+}
